7c4341a37fea78916bec4aee0b0c5562b248a9c4
Bejan Maljai <bmaljai@gmail.com>
2016-10-19 07:35:19 -0700

    added parser and parse_checker methods to filter out rows that have extra info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,58 @@
 require "csv"
 class PagesController < ApplicationController
   def index
-    @parsed_file = CSV.read("data.tsv", {:col_sep => "\t"})
-    
+    @parsed_file = parse_tsv("data.tsv")
+    @parsed_file2 = parse_tsv("data2.tsv")
+    #@normalized = normalize_from_parse(@parsed_file)
+
+  end
+
+  private
+
+  def parse_tsv(file_name)
+    tsv_array = CSV.read("#{file_name}", {:col_sep => "\t"})
+    if check_parse(tsv_array)
+      return check_parse(tsv_array)
+    else
+      return tsv_array
+    end
+  end
+
+  def check_parse(parsed_file_array)
+    field_count = parsed_file_array.first.count
+    problem_row_hash = {}
+    parsed_file_array.each_with_index do |parsed_item, index|
+      if parsed_item.count <= field_count
+        next
+      else
+        problem_row_hash["#{index}"] = parsed_item
+      end
+    end
+
+    if problem_row_hash.empty?
+      return false
+    else
+      return problem_row_hash
+    end
+  end
+
+  def normalize_company(parsed_file_array)
+    companies = Hash.new(0)
+    parsed_file_array.each do |parsed_item|
+      companies["#{parsed_item[-1]}"] += 1
+    end
+    return companies
+  end
+
+  def normalize()
+    company_keys = Hash.new(0)
+    email_keys = Hash.new(0)
+    parsed_file_array.each do |parsed_item|
+      company_keys << parsed_item[-1]
+    end
   end
 end
+
+#create before action for create normalization
+# or helper method
+# can take row number in reading/parsing to return error if problem
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+    def check_parse(parsed_file_array)
+    field_count = parsed_file_array.first.count
+    problem_row_hash = {}
+    parsed_file_array.each_with_index do |parsed_item, index|
+      if parsed_item.count <= field_count
+        puts "do next step of normalization"
+      else
+        problem_row_hash["#{index}"] = parsed_item
+      end
+    end
+    return problem_row_hash
+  end
 end
