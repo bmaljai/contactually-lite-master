e50131d5cccbe29172d8b977c977e7c8b4be8211
Bejan Maljai <bmaljai@gmail.com>
2016-10-24 15:49:42 -0700

    added styles and changed controllers/refactored normalization

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,84 @@
+require "csv"
 class ContactsController < ApplicationController
   def index
     @contacts = User.all
   end
 
   def create
-
+    parsed_file = parse_tsv(params[:file])
+    parsed_file[1..-1].each do |user_entry|
+      update_or_create_user(user_entry)
+    end
+    redirect_to "/contacts"
   end
 
   def destroy
     user = User.find(params[:id]).delete
     render json: user
+  end
+
+  private
+
+  def parse_tsv(file)
+    myfile = file
+    tsv_array = CSV.read(myfile.path, {:col_sep => "\t"})
+    if check_parse(tsv_array)
+      return check_parse(tsv_array)
+    else
+      return tsv_array
+    end
+  end
+
+  def check_parse(parsed_file_array)
+    field_count = parsed_file_array.first.count
+    problem_row_hash = {}
+    parsed_file_array.each_with_index do |parsed_item, index|
+      if parsed_item.count <= field_count
+        next
+      else
+        problem_row_hash["#{index}"] = parsed_item
+      end
+    end
+    if problem_row_hash.empty?
+      return false
+    else
+      return problem_row_hash
+    end
+  end
+
+
+  def find_or_create_company(company_name)
+    return Company.find_by(name: company_name).try(:id) || Company.create(name: company_name).id
+  end
+
+  def update_or_create_user(user_array)
+    company_id = find_or_create_company(user_array[4])
+    user = User.find_by(email_address: user_array[2])
+    if user
+      user.update(first_name: user_array[0], last_name:user_array[1], phone_number: normalize_phone(user_array[3]), company_id: company_id)
+    else
+      User.create(first_name: user_array[0], last_name: user_array[1], email_address: user_array[2], phone_number: normalize_phone(user_array[3]), company_id: company_id)
+    end
+  end
+
+  def split_phone(string_phone)
+    split_string_phone = string_phone.split(/[(, ), ., -]/)
+    if split_string_phone[0] == ""
+      return split_string_phone[1..-1]
+    else
+      return split_string_phone
+    end
+  end
+
+  def join_phone(array_phone)
+    if array_phone[-1].include?("x")
+      return "#{array_phone[0..-2].join('-')} #{array_phone[-1]}"
+    else
+      return "#{array_phone.join('-')}"
+    end
+  end
+
+  def normalize_phone(string_phone)
+    return join_phone(split_phone(string_phone))
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,29 +1,4 @@
 module PagesHelper
-    def check_parse(parsed_file_array)
-    field_count = parsed_file_array.first.count
-    problem_row_hash = {}
-    parsed_file_array.each_with_index do |parsed_item, index|
-      if parsed_item.count <= field_count
-        puts "do next step of normalization"
-      else
-        problem_row_hash["#{index}"] = parsed_item
-      end
-    end
-    return problem_row_hash
-  end
-
-  #   def normalize_phone(string_phone)
-  #   chars = ["(", ")", ".", "-"]
-  #   new_string = ""
-  #   string_phone.split("").each do |string_phone_char|
-  #     if chars.include?(string_phone_char)
-  #       next
-  #     else
-  #       new_string += string_phone_char
-  #     end
-  #   end
-  #   return new_string
-  # end
 
   def normalize_phone(string_phone)
     split_string_phone = string_phone.split(/[(, ), ., -]/)
@@ -41,24 +16,4 @@ module PagesHelper
       return "#{array_phone.join('-')}"
     end
   end
-
-  # def extensionify(string_phone)
-  #   string_phone = string_phone.split("x")
-  #   phone_number = ""
-  #   extension = "Ext. "
-  #   string_phone.each_with_index do |string, index|
-  #     case index
-  #     when 0
-  #         string.insert(-5, "-")
-  #         string.insert(-9, "-")
-  #       if string.length > 12
-  #         string.insert(-13, "-")
-  #       end
-  #       phone_number = string
-  #     when 1
-  #       extension += string
-  #     end
-  #   end
-  #   return "#{phone_number} #{extension if extension.length > 5}"
-  # end
 end
